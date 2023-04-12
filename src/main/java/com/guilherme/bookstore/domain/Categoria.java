abd03c021d0253374c55697a16e44fda56ef6de5
GuiLagunde <guilherme.lagunde@universo.univates.br>
2023-04-12 15:39:11 -0300

    finalizando classes de dominio

--- src/main/java/com/guilherme/bookstore/domain/Categoria.java
+++ src/main/java/com/guilherme/bookstore/domain/Categoria.java
@@ -1,21 +1,31 @@
 package com.guilherme.bookstore.domain;
 
+import javax.persistence.*;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class Categoria {
+@Entity
+public class Categoria implements Serializable{
 
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private String descricao;
 
+    @OneToMany(mappedBy = "categoria")
     private List<Livro> livros= new ArrayList<>();
 
     public Categoria() {
+        super();
     }
 
     public Categoria(Integer id, String nome, String descricao) {
+       super();
         this.id = id;
         this.nome = nome;
         this.descricao = descricao;
