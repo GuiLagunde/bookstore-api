75bab872b6eac362bf49ec0440639f24b3ef2e24
GuiLagunde <https://GuiLagunde@github.com>
2023-04-17 17:28:47 -0300

    update e patch livro

--- src/main/java/com/guilherme/bookstore/resources/LivroResource.java
+++ src/main/java/com/guilherme/bookstore/resources/LivroResource.java
@@ -1,13 +1,14 @@
 package com.guilherme.bookstore.resources;
 
 import com.guilherme.bookstore.domain.Livro;
+import com.guilherme.bookstore.dtos.LivroDto;
 import com.guilherme.bookstore.service.LivrosService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/livros")
@@ -21,4 +22,24 @@ public class LivroResource {
         Livro livro = livrosService.findById(id);
         return ResponseEntity.ok().body(livro);
     }
+
+    @GetMapping
+    public ResponseEntity<List<LivroDto>> findAll(@RequestParam(value="categoria", defaultValue = "0")Integer id_cat){
+        List<Livro> list = livrosService.findAll(id_cat);
+        List<LivroDto> listDto = list.stream().map(obj -> new LivroDto(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Livro> update(@PathVariable Integer id,@RequestBody Livro obj){
+        Livro newObj = livrosService.update(id,obj);
+        return ResponseEntity.ok().body(newObj);
+    }
+
+    @PatchMapping(value = "/{id}")
+    public ResponseEntity<Livro> updatePatch(@PathVariable Integer id,@RequestBody Livro obj){
+        Livro newObj = livrosService.update(id,obj);
+        return ResponseEntity.ok().body(newObj);
+    }
+
 }
--- src/main/java/com/guilherme/bookstore/service/LivrosService.java
+++ src/main/java/com/guilherme/bookstore/service/LivrosService.java
@@ -7,6 +7,7 @@ import com.guilherme.bookstore.service.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -15,9 +16,29 @@ public class LivrosService {
     @Autowired
    private LivroRepository livroRepository;
 
+    @Autowired
+    private CategoriaService categoriaService;
+
     public Livro findById(Integer id){
 
         Optional<Livro> obj = livroRepository.findById(id);
         return obj.orElseThrow(()-> new ObjectNotFoundException(" Objeto não encontrado! Id: " + id + ", Tipo: " + Livro.class.getName()));
+    }
+
+    public List<Livro> findAll(Integer idCat) {
+        categoriaService.findById(idCat);
+        return  livroRepository.findAllByCategoria(idCat);
+    }
+
+    public Livro update(Integer id, Livro obj) {
+        Livro newObj = findById(id);
+        updateData(newObj, obj);
+        return livroRepository.save(newObj);
+    }
+
+    private void updateData(Livro newObj, Livro obj) {
+        newObj.setTitulo(obj.getTitulo());
+        newObj.setNome_autor(obj.getNome_autor());
+        newObj.setTexto(obj.getTexto());
     }
 }
